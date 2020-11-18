2f28810f235e17132e9b25e13b0b147be1485e6a
MATHEUS DE CARVALHO FANTONI <p808664@d02.prodemge.gov.br>
2020-11-18 15:54:13 -0300

    listando registros com cakephp

---
+++ src/Controller/UsersController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+class UsersController extends AppController{
+
+    public function index(){
+
+        //$usuario = "Matheus";
+        //$this->set(['usuarios' => $usuario]);
+
+        $usuarios = $this->Users->find()->all();
+        //$this->set(['usuarios' => $usuarios]);
+        $this->set(compact('usuarios'));
+
+    }
+
+}
---
+++ src/Model/Entity/User.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Model\Entity;
+
+use Cake\ORM\Entity;
+
+class User extends Entity{
+
+    public $_accessible = [
+        'id' => true,
+        'name' => true,
+        'email' => true,
+        'username' => true,
+        'password' => true,
+        'created' => true,
+        'modified' => true
+
+    ];
+}
+
